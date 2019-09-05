9dad4226046d784623cd0def77f5d8a7cddb77c9
subhasri jayamala <subhasri_guru@yahoo.com>
2019-09-05 11:26:49 +0530

    after adding second line , save it in remote repo

--- GitDemoProject/src/com/manipal/Demo.java
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello Git");
+		System.out.println("hello Git ....");
+		System.out.println("second line added");
 	}
 
 }
