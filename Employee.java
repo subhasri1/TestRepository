23597db637d853a1f73723033da904490731a717
subhasri1 <50294534+subhasri1@users.noreply.github.com>
2019-09-05 11:41:21 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package com.manipal.beans;
+
+public class Employee {
+	private int eno;
+	private String ename;
+	private float bp;
+	
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public float getBp() {
+		return bp;
+	}
+	public void setBp(float bp) {
+		this.bp = bp;
+	}
+	
+	
+
+}
